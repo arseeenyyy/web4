598252a096b2a18b154d2efcf3bda89e34dd05e6
arseeenyyy <rubtsov113@mail.ru>
2025-01-13 12:43:27 +0300

    add server validation

--- backend/web4/src/main/java/com/github/arseeenyyy/utils/Validator.java
+++ backend/web4/src/main/java/com/github/arseeenyyy/utils/Validator.java
@@ -1,5 +1,16 @@
 package com.github.arseeenyyy.utils;
 
 public class Validator {
-    
+    public static boolean validateArgs(double x, double y, int r) {
+        return validateX(x) && validateY(y) && validateR(r);
+    }
+    private static boolean validateX(double x) {
+        return x >= -5 && x <= 3;
+    }
+    private static boolean validateY(double y) {
+        return y >= -5 && y <= 5;
+    }
+    private static boolean validateR(int r) {
+        return r > 0 && r <= 5;
+    }
 }
